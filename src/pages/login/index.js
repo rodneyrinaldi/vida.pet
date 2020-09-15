f530619243e6404a6270768d77c50ec4f4911dbd
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2020-09-15 02:59:13 -0300

    feat: login page design

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,4 +1,6 @@
+import Link from 'next/link';
 import { useState, FormEvent } from 'react';
+import { File } from '@styled-icons/boxicons-regular/File'
 import styles from './index.module.css'
 
 export default function Login() {
@@ -10,8 +12,9 @@ export default function Login() {
   }
 
   return (
-    <section className="sectionWhiteTier">
+    <section className="sectionEmptyTier">
       <section className={styles.sectionPagesImg}>
+        <h1>vida.pet</h1>
         <img src="/banner-pages.png" alt="logo" />
       </section>
       <form method="post" action="#" className={styles.sectionLogin} >
@@ -21,6 +24,14 @@ export default function Login() {
         <input type="password" name="name" placeholder="Senha" value={password}
           onChange={(e) => { setPassword(e.target.value) }}
         />
+        <Link href="/signin">
+          <a>
+            <p>
+              <File className={styles.iconLink} />
+              {' '} cadastrar novo usuário
+            </p>
+          </a>
+        </Link>
         <section className={styles.sectionLoginButtons}>
           <input type="submit" className={styles.buttonsPrimary} value="ENTRAR" />
           <input type="submit" className={styles.buttonsPrimary} value="VOLTAR" />
