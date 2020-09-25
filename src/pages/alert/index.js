428151625fc2821188977e8c5341b62192fc5353
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2020-09-25 02:08:11 -0300

    feat: embedded google forms

--- src/pages/alert/index.js
+++ src/pages/alert/index.js
@@ -1,3 +1,11 @@
+import styles from './index.module.css'
+
 export default function Alert() {
-  return (<><p>Formulario de alerta de pet encontrato</p></>)
+  return (
+    <div className={styles.sectionFormTier}>
+      <p>Formulario de alerta de pet encontrato</p>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeRIY6NrKBRfu4RoCcJXqUWL3459S7nE3bOIueY-Tn-x-kjQw/viewform?embedded=true">
+        Loading…
+      </iframe>
+    </div>)
 }
